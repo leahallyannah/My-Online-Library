25bb737e4d423ea67fb4b8cd3af438c562c0257a
leahallyannah <39140433+leahallyannah@users.noreply.github.com>
2018-05-30 19:26:33 -0400

    Add files via upload

---
+++ LEAHS ONLINE LIBRARY AGAIN/inc/data.php
@@ -0,0 +1,165 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Divergent",
+	"img" => "img/media/Book1.jpg",
+    "genre" => "Young Adult Novel",
+    "format" => "Paperback",
+    "year" => 2012,
+    "category" => "Books",
+    "authors" => [
+        "Suzanne Collins"
+    ],
+    "publisher" => "Penguin",
+    "isbn" => '9781594137907'
+];
+$catalog[102] = [
+    "title" => "The Hunger Games",
+    "img" => "img/media/Book2.jpg",
+    "genre" => "Mystery",
+    "format" => "Paperback",
+    "year" => 2003,
+    "category" => "Books",
+    "authors" => [
+        "Mark Haddon"
+    ],
+    "publisher" => "Doubleday",
+    "isbn" => '9781101911617'
+];
+$catalog[103] = [
+    "title" => "The Fault in Our Stars",
+    "img" => "img/media/Book3.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 1964,
+    "category" => "Books",
+    "authors" => [
+        "John Green"
+
+    ],
+    "publisher" => "Jean-Jacques Sempé",
+    "isbn" => '978-0201485677'
+];
+$catalog[104] = [
+    "title" => "IT",
+    "img" => "img/media/Book4.jpg",
+    "genre" => "Fable",
+    "format" => "Paperback",
+    "year" => 1943,
+    "category" => "Books",
+    "authors" => [
+        "Stephen King"
+    ],
+    "publisher" => "Antoine de Saint-Exupéry",
+    "isbn" => '978-0822594222'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Black Panther",
+    "img" => "img/media/Movie1.jpg",
+    "genre" => "Adventure",
+    "format" => "DVD",
+    "year" => 2016,
+    "category" => "Movies",
+    "director" => "Ryan Coogler",
+    "writers" => [
+        "Andrew Stanton",
+        "Angus MacLane"
+    ],
+    "stars" => [
+        "Ellen DeGeneres",
+        "Albert Brooks",
+        "Ed O'Neill",
+        "Kaitlin Olson"
+    ]
+];
+$catalog[202] = [
+    "title" => "Deadpool",
+    "img" => "img/media/Movie2.jpg",
+    "genre" => "Fantasy/Science fiction film",
+    "format" => "Blu-ray",
+    "year" => 2018,
+    "category" => "Movies",
+    "director" => "idk cari i had to start over",
+    "writers" => [
+        "Ryan Coogler"
+    ],
+    "stars" => [
+        "Chadwick Boseman",
+        "Leah Allyannah",
+        "Lupita Nyong'o",
+        "Danai Gurira",
+        "Daniel Kaluuya",
+        "Forest Whitaker"
+    ]
+];
+$catalog[203] = [
+    "title" => "Moana",
+    "img" => "img/media/Movie3.jpg",
+    "genre" => "Comedy",
+    "format" => "Blu-ray",
+    "year" => 2018,
+    "category" => "Movies",
+    "director" => "Dany Boon",
+    "writers" => [
+        "Dany Boon"
+    ],
+    "stars" => [
+        "Dwayne The Rock Johnson"
+	
+    ]
+];
+$catalog[204] = [
+    "title" => "The Greatest Showman",
+    "img" => "img/media/Movie4.jpg",
+    "genre" => "Drama/Mystery",
+    "format" => "DVD",
+    "year" => 2015,
+    "category" => "Movies",
+    "director" => "Jake Schreier",
+    "writers" => [
+        "Hugh Jackman"
+    ],
+    "stars" => [
+        "Hugh Jackman"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Imagine Dragons",
+    "img" => "img/media/Music3.jpg",
+    "genre" => "Pop",
+    "format" => "MP3",
+    "year" => 2018,
+    "category" => "Music",
+    "artist" => "Imagine Dragons"
+];
+$catalog[302] = [
+    "title" => "Sweet Sexy Savage",
+    "img" => "img/media/Music1.jpg",
+    "genre" => "Future bass, Dance/electronic",
+    "format" => "MP3",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "Kehlani"
+];
+$catalog[303] = [
+    "title" => "Black Panther",
+    "img" => "img/media/Music2.jpg",
+    "genre" => "Pop music, Rock music, Blues",
+    "format" => "MP3",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "Various Artists"
+];
+$catalog[304] = [
+    "title" => "The Life Of Pablo",
+    "img" => "img/media/Music4.jpg",
+    "genre" => "Pop",
+    "format" => "MP3",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Kanye West"
+];
+?> 
---
+++ LEAHS ONLINE LIBRARY AGAIN/inc/footer.php
@@ -0,0 +1,19 @@
+</div><!-- end content -->
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>
+				<li><a href="https://www.instagram.com/LifeWithErick/">Twitter</a></li>
+				<li><a href="https://www.facebook.com/LifeWithErick">Facebook</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date("Y"); ?> My Online Library Collection</p>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
